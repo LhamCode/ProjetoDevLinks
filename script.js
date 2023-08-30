e27a5ce126a566657f2925c0cc01a95bf4cc0d6d
Luã Moura <lmoura.ha@gmail.com>
2023-08-30 17:39:38 -0300

    :tada: initial commit

---
+++ script.js
@@ -0,0 +1,23 @@
+function toggleMode() {
+  const html = document.documentElement
+  const img = document.querySelector("#profile img")
+
+  if (html.classList.contains("light")) {
+    html.classList.remove("light")
+    img.setAttribute('src', './assets/assets/avatar.png')
+    img.setAttribute('alt', 'luã com camisa social e gravata')
+  } else {
+    html.classList.add("light")
+    img.setAttribute('src', './assets/assets/avatar-light.png')
+    img.setAttribute("alt", "luã sério de óculos escuro")
+  }
+
+  //pegar a tag img//
+   
+  /* Esses condicionais poderiam ser substituídos por 
+  html.classList.toogle('light') */
+
+  //se tiver ligth mode, adicionar a imagem light//
+  //se tiver sem ligth mode, manter a imagem normal//
+  //substituir a imagem//
+}
